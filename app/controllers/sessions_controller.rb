230314089738d9c1d694458909588a820097a10f
Matt Wylder <matt@wylder.us>
2016-07-03 12:52:23 -0500

    added persistent sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
       user = User.find_by(name: params[:session][:name].downcase)
       if user && user.authenticate(params[:session][:password])
           log_in user
+          remember user
           redirect_to user
       else
           flash[:danger] = 'bill dont like'
