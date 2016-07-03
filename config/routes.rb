f6233763c9f6e6c3ccd314284cafc86812727568
Matt Wylder <matt@wylder.us>
2016-07-02 22:13:20 -0500

    created session controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :users
   get 'home/construction'
   root 'home#construction'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete 'logout',   to: 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
