b18913f195aaf74c57d6d622a4ca3de4faf1035d
Matt Wylder <matt@wylder.us>
2016-07-02 23:00:00 -0500

    added loggin view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+      user = User.find_by(name: params[:session][:email].downcase)
+      if user && user.authenticate(params[:session][:password])
+          #log in and show user 
+      else
+          flash[:danger] = 'bill dont like'
+          render 'new'
+      end
+  end
+
+  def destroy
+  end
 end
