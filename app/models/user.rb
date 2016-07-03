2544b3705fc1587b2728cff2d4503694342887e6
Matt Wylder <matt@wylder.us>
2016-07-03 12:01:20 -0500

    rememer digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
     validates :name, presence: true, uniqueness: true
     has_secure_password
+    
+    # Returns the hash digest of the given string
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                      BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+    end
 end
