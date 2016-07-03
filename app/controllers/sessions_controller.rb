d5b4775ade1c9222503e147cd982c1b1e42700c4
Matt Wylder <matt@wylder.us>
2016-07-03 13:11:25 -0500

    forget persistent users on logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
       user = User.find_by(name: params[:session][:name].downcase)
       if user && user.authenticate(params[:session][:password])
           log_in user
-          remember user
+          params[:session][:remember_me] == '1' ? remember(user) : forget(user)
           redirect_to user
       else
           flash[:danger] = 'bill dont like'
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-      log_out
+      log_out if logged_in?
       redirect_to root_url
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+
     attr_accessor :remember_token
     validates :name, presence: true, uniqueness: true
     has_secure_password
@@ -10,17 +11,25 @@ class User < ActiveRecord::Base
         BCrypt::Password.create(string, cost: cost)
     end
 
+    # returns a random token
     def User.new_token
         SecureRandom.urlsafe_base64
     end
 
+    # remembers a user in the database for use in persistent sessions
     def remember
         self.remember_token = User.new_token
         update_attribute(:remember_digest, User.digest(remember_token))
     end
 
+    # forgets a user
+    def forget
+        update_attribute(:remember_digest, nil)
+    end
+
     # Returns true if the given token matches the digest
     def authenticated?(remember_token)
+        return false if remember_digest.nil?
         BCrypt::Password.new(remember_digest).is_password?(remember_token)
     end
 end
