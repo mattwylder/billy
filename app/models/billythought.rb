fe8f079232e59e2f5606c182abfa460fadc1031c
Matt Wylder <matt@wylder.us>
2016-07-04 22:58:53 -0500

    something:

--- app/models/billythought.rb
+++ app/models/billythought.rb
@@ -11,25 +11,70 @@ class Billythought < ActiveRecord::Base
 
   def get_background_color
       diff = namastes - doodooblasts
-      if diff == 0
-          color = 0xfff
-      elsif diff > 0
-          color = namaste_color(diff)
-      else
-          color = doodooblast_color(diff)
-      end
-      color.to_s(16)
+      get_rgb(diff)
   end
 
   private
 
-  def namaste_color(diff)
+  def get_rgb(diff)
+      color = diff > 0 ? namaste_rgb(diff) : doodooblast_rgb(diff)
+      "#" + color
+  end
+
+  def namaste_rgb(diff)
       color = @@white - diff * @@pink_increment
       color = color > @@darkest_pink ? color : @@darkest_pink
+      color.to_s(16)
   end
 
-  def doodooblast_color(diff)
-      color = 0x4d3319
+  def doodooblast_rgb(diff)
+      color = hsl_to_rgb(30.0,50.0,40.0)
   end
 
+  def get_hsl(diff)
+      color = diff > 0 ? namaste_hsl(diff) : doodooblast_hsl(diff)
+      "hsl(" + color + ")"
+  end
+
+  def namaste_hsl(diff)
+  end
+
+  def doodooblast_hsl(diff)
+      color = "27, 50, 65"
+  end
+
+  def hsl_to_rgb(h,s,l)
+      if s == 0
+          r = g = b = l
+      else
+          q = l < 0.5 ? l * (1 + s) : l + s - l * s
+          p = 2 * l - q
+          r = hue_to_rgb(p,q,h + 1/3)
+          g = hue_to_rgb(p,q,h)
+          b = hue_to_rgb(p,q, h - 1/3)
+      end
+      r *= 255
+      g *= 255
+      b *= 255
+     return r.round.to_s + g.round.to_s + b.round.to_s
+  end
+
+  def hue_to_rgb(p,q,t)
+      if t < 0 
+          t += 1
+      end
+      if t > 1
+          t -= 1
+      end
+      if t < 1/6 
+          return p + (q - p) * 6  * t
+      end
+      if t < 1/2 
+         return q
+      end
+      if t < 2/3
+          return p + (q - p) * (2/3 -t) * 6
+      end
+      return p
+  end
 end
